31b9f917bb909052a2963209a3d94c1f03fd0a53
daemonCome <1070630668@qq.com>
2019-08-14 23:43:57 +0800

    1
    1

--- untitled/hello.py
+++ untitled/hello.py
@@ -11,6 +11,10 @@ def print_time( threadName, delay):
       count += 1
       print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
 
+
+
+
+
 # 创建两个线程
 try:
    _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
